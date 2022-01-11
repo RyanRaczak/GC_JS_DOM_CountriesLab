599e1eec51aa3278a017769f2511fcd46f98a8a9
Ryan Raczak <ryanraczak@rocketmortgage.com>
2022-01-11 16:18:50 -0500

    Added flags

--- country.js
+++ country.js
@@ -1,9 +1,10 @@
 class Country {
-    constructor(name, lang, greeting, colors) {
+    constructor(name, lang, greeting, colors, flag) {
         this.name = name;
         this.lang = lang;
         this.greeting = greeting;
         this.colors = colors;
+        this.flag = flag;
     }
 
     DisplayColors(){
@@ -16,13 +17,14 @@ class Country {
         document.getElementById("CountryName").innerText = this.name;
         document.getElementById("OfficialLanguage").innerText = this.lang;
         document.getElementById("HelloWorld").innerText = this.greeting;
+        document.getElementById("CountryFlag").src = this.flag;
     }
 }
-let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "blue", "white"]);
-let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"]);
-let japan = new Country("Japan", "Japanese", "Kon'nichiwa sekai \n こんにちは世界", ["red", "black", "white"]);
-let germany = new Country("Germany", "German", "Hallo Welt", ["black", "red", "gold"]);
+let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "blue", "white"], "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/220px-Flag_of_the_United_States.svg.png");
+let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"], "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/255px-Flag_of_Mexico.svg.png");
+let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"], "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_Algeria.svg/255px-Flag_of_Algeria.svg.png");
+let japan = new Country("Japan", "Japanese", "Kon'nichiwa sekai \n こんにちは世界", ["red", "black", "white"],"https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/255px-Flag_of_Japan.svg.png");
+let germany = new Country("Germany", "German", "Hallo Welt", ["black", "red", "gold"],"https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/255px-Flag_of_Germany.svg.png");
 
 
 function SwitchCountry() {
